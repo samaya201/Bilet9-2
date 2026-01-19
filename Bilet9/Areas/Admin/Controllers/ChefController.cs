526fd80178b967e6154c69ec6024c146802c313f
Samaya Aliyeva <aliyevasamaya33@gmail.com>
2026-01-20 00:29:42 +0400

    Crud Added2

--- Bilet9/Areas/Admin/Controllers/ChefController.cs
+++ Bilet9/Areas/Admin/Controllers/ChefController.cs
@@ -87,19 +87,87 @@ public class ChefController : Controller
 
     public async Task<IActionResult> Update(int id)
     {
+        await SendPositionWithViewBag();
         var chef = await _context.Chefs.FindAsync(id);
         if (chef is null)
             return NotFound();
 
         ChefUpdateVM vm = new() 
         { 
+            Id=chef.Id,
+            Name=chef.Name,
+            PositionId=chef.PositionId,
             
-             
         };
 
         return View(vm);
     }
 
+    [HttpPost]
+    public async Task<IActionResult> Update(ChefUpdateVM vm)
+    {
+        await SendPositionWithViewBag();
+        if (!ModelState.IsValid)
+        {
+            return View(vm);
+        }
+
+        //Find chef for update
+        var isExistChef=await _context.Chefs.FindAsync(vm.Id);
+        if(isExistChef is null)
+            return NotFound();
+
+        //Check position
+        var isPositionExist = await _context.Positions.AnyAsync(x => x.Id == vm.PositionId);
+        if (!isPositionExist)
+        {
+            return NotFound();
+        }
+
+        //Check image size
+        if (!vm.Image?.CheckSize(2) ?? false)
+        {
+            ModelState.AddModelError("Image", "Image size  must be less than 2 mb");
+            return View(vm);
+        }
+        //Check file type
+        if (!vm.Image?.CheckType("image") ?? false)
+        {
+            ModelState.AddModelError("Image", "File must be image type");
+            return View(vm);
+        }
+        //Update
+        isExistChef.Name = vm.Name;
+        isExistChef.PositionId = vm.PositionId;
+
+        if(vm.Image is { })
+        {
+            string newImagePath = await vm.Image.FileUploadToAsync(_folderPath);
+            string deletePath = Path.Combine(_folderPath, isExistChef.ImagePath);
+            FileHelper.DeleteFile(deletePath);
+            isExistChef.ImagePath = newImagePath; 
+        }
+        _context.Chefs.Update(isExistChef);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var chef = await _context.Chefs.FindAsync(id);
+        if(chef is null)
+            return NotFound();
+        _context.Chefs.Remove(chef);
+        await _context.SaveChangesAsync();
+
+        //Delete image
+
+        string deletedPath = Path.Combine(_folderPath, chef.ImagePath);
+        FileHelper.DeleteFile(deletedPath);
+
+        return RedirectToAction("Index");
+    }
     private async Task SendPositionWithViewBag()
     {
         var position = await _context.Positions.ToListAsync();
---
+++ Bilet9/Areas/Admin/Controllers/PositionController.cs
@@ -0,0 +1,67 @@
+﻿using Bilet9.Context;
+using Bilet9.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Bilet9.Areas.Admin.Controllers;
+[Area("Admin")]
+
+public class PositionController(AppDbContext _context) : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        var positions = await _context.Positions.ToListAsync();
+        return View(positions);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(Position position)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(position);
+        }
+
+        Position newPosition = new() 
+        {
+            Title = position.Title
+        };
+        _context.Positions.Add(newPosition);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+
+    }
+    public async Task<IActionResult> UpdateAsync(int id)
+    {
+        var position =  await _context.Positions.FindAsync(id);
+        return View(position);
+
+    }
+    [HttpPost]
+    public async Task<IActionResult> Update(Position position)
+    {
+        var isExistPosition = await _context.Positions.FindAsync(position.Id);
+        if (isExistPosition is null)
+            return NotFound();
+        isExistPosition.Title = position.Title;
+
+        _context.Positions.Update(isExistPosition);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var position = await _context.Positions.FindAsync(id);
+        if(position is null)
+            return NotFound();
+        _context.Positions.Remove(position);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+}
